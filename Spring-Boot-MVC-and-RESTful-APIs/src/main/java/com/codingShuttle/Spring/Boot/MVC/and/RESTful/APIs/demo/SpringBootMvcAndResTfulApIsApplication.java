6b7b6c118ec5df4f6ff5ce2353a036eeaf82cfcf
saurabh khandale <saurabhkhandale3991@gmail.com>
2026-07-21 19:33:06 +0530

    chapter 2.1

---
+++ Spring-Boot-MVC-and-RESTful-APIs/src/main/java/com/codingShuttle/Spring/Boot/MVC/and/RESTful/APIs/demo/SpringBootMvcAndResTfulApIsApplication.java
@@ -0,0 +1,13 @@
+package com.codingShuttle.Spring.Boot.MVC.and.RESTful.APIs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcAndResTfulApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcAndResTfulApIsApplication.class, args);
+	}
+
+}
---
+++ Spring-Boot-MVC-and-RESTful-APIs/src/test/java/com/codingShuttle/Spring/Boot/MVC/and/RESTful/APIs/demo/SpringBootMvcAndResTfulApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingShuttle.Spring.Boot.MVC.and.RESTful.APIs.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcAndResTfulApIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
